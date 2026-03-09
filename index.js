44a3bc5d83e9677fe10115349ac5a2d86c778354
shaw8386 <it.marketing.loki@gmail.com>
2026-03-10 00:38:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
 // ============================================================
